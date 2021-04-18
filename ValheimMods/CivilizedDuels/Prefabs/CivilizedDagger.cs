b305fa72838eef31bc3aa723d515cdc4f11c41df
DickDangerJustice <dickdangerjustice@gmail.com>
2021-04-18 15:18:51 -0400

    Create status effect.

--- ValheimMods/CivilizedDuels/Prefabs/CivilizedDagger.cs
+++ ValheimMods/CivilizedDuels/Prefabs/CivilizedDagger.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using CivilizedDuels.StatusEffects;
+using HarmonyLib;
 using JotunnLib.Entities;
 using UnityEngine;
 
@@ -15,27 +16,12 @@ namespace ExampleMagicBox.Prefabs
         public override void Register()
         {
             // Configure item drop
-            // ItemDrop is a component on GameObjects which determines info about the item when it's picked up in the inventory
             ItemDrop item = Prefab.GetComponent<ItemDrop>();
-            item.m_itemData.m_shared.m_name = "Magic Armor";
-            item.m_itemData.m_shared.m_description = "Godlike armor with a twist";
-            item.m_itemData.m_shared.m_armor = 999;
+            item.m_itemData.m_shared.m_name = "Civilized Dagger";
+            item.m_itemData.m_shared.m_description = "Start civilized duels";
 
-            // create status effect
-            var burning = ScriptableObject.CreateInstance<SE_Burning>();
-
-            // load damages field
-            var burnDamageField = AccessTools.Field(typeof(SE_Burning), "m_damage");
-            var burnDamage = (HitData.DamageTypes)burnDamageField.GetValue(burning);
-
-            // edit fire damage
-            burnDamage.m_fire = 1;
-
-            // save damages field
-            burnDamageField.SetValue(burning, burnDamage);
-
-            // set item status effect
-            item.m_itemData.m_shared.m_equipStatusEffect = burning;
+            // set challenged status effect
+            item.m_itemData.m_shared.m_attackStatusEffect = ScriptableObject.CreateInstance<SE_Challenged>();
         }
     }
 }
---
+++ ValheimMods/CivilizedDuels/StatusEffects/SE_Challenged.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CivilizedDuels.StatusEffects
+{
+    class SE_Challenged : StatusEffect
+    {
+        //[Header("SE_Challenged")]
+
+        public new string name;
+
+        public override void Setup(Character character)
+        {
+            base.Setup(character);
+        }
+
+        public override void UpdateStatusEffect(float dt)
+        {
+            base.UpdateStatusEffect(dt);
+        }
+    }
+}
